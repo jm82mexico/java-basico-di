fc220c90ab96ea0d47ad5f384abca40fd3fbcc4c
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-05-04 14:26:34 -0600

    commit inicial

---
+++ src/main/java/com/seccion3/seccion3web/Seccion3WebApplication.java
@@ -0,0 +1,13 @@
+package com.seccion3.seccion3web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seccion3WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seccion3WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seccion3/seccion3web/Seccion3WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seccion3.seccion3web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seccion3WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
