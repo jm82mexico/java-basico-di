0bbdf167aa5adc681750262cf57a377e6e11b1e7
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-05-05 16:24:52 -0600

    refactor: Desacoplar ProductServiceImpl de la implementación concreta de ProductRepository mediante inyección de dependencias

--- src/main/java/com/seccion3/seccion3web/services/ProductServiceImpl.java
+++ src/main/java/com/seccion3/seccion3web/services/ProductServiceImpl.java
@@ -3,8 +3,6 @@ package com.seccion3.seccion3web.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 import com.seccion3.seccion3web.models.Product;
